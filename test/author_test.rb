9f246b32dd3007fd0a4f3105152f0e001735933a
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-02-13 09:15:44 -0700

    Adds working book and book test, part of author test

--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,14 @@
 require './test/test_helper'
+require './lib/author'
 
 class AuthorTest < Minitest::Test
 
+  def setup
+    @nk_jemisin = Author.new({first_name: "N.K.", last_name: "Jemisin"})
+  end
+
+  def test_it_exists
+    assert_instance_of Author, @nk_jemisin
+  end 
 
 end
