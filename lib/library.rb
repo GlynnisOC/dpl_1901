257eaa6635adc0b99c55bb89e69b09084b788720
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-02-13 10:39:35 -0700

    Edited library class

--- lib/library.rb
+++ lib/library.rb
@@ -1,8 +1,7 @@
 class Library
-  attr_reader :author, :books
+  attr_reader :books
   def initialize
     @books = [] #this doesn't seem like SRP (as books is included in the author class), please correct me if I'm wrong!
-    @author = author
   end
 
   def add_to_collection(book_info)
